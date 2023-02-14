e18a40da3db9c90329922db95b7b2add5361fe96
Denys Zosimovych <unsinedz.work@gmail.com>
2023-02-14 13:56:09 +0100

    Change welcome message

--- client/client.go
+++ client/client.go
@@ -6,19 +6,19 @@ import (
 
 var version string
 
-//Client struct base
+// Client struct base
 type Client struct {
 }
 
-//NewClient return a new Client instance
+// NewClient return a new Client instance
 func NewClient(_version string) *Client {
 	version = _version
 	return &Client{}
 }
 
-//Run starts client settings and run
+// Run starts client settings and run
 func (client *Client) Run() error {
-	log.Infof("Starting Albion Data Client, version: %s", version)
+	log.Infof("Starting Albion Private Prices Client, version: %s", version)
 	log.Info("This is a third-party application and is in no way affiliated with Sandbox Interactive or Albion Online.")
 	log.Info("Additional parameters can listed by calling this file with the -h parameter.")
 
